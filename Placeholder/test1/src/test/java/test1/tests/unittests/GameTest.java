d228f895177e6ee77038be8f5363101267593418
Daniel Andrade Martins <dan.andrade@ua.pt>
2025-12-05 13:02:36 +0000

    Commit 5 - GameTest Unittest

---
+++ Placeholder/test1/src/test/java/test1/tests/unittests/GameTest.java
@@ -0,0 +1,37 @@
+package test1.tests.unittests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import test1.test1.model.Game;
+
+
+public class GameTest {
+
+    @Test
+    void testGameConstructorAndGetters(){
+        Game game = new Game("Super Mario","Super Mario Party",4.0);
+        assertEquals("Super Mario", game.getTitle());
+        assertEquals("Super Mario Party", game.getDescription());
+        assertEquals(4.0, game.getPricePerDay());
+        assertTrue(game.isAvailable());
+    }
+
+    @Test
+    void testGameSetters(){
+        Game game = new Game("Super Mario","Super Mario Party",4.0);
+        game.setTitle("Among Us");
+        game.setAvailable(false);
+        game.setDescription("Space killer game");
+        game.setPricePerDay(1.2);
+        assertEquals("Among Us", game.getTitle());
+        assertEquals("Space killer game", game.getDescription());
+        assertEquals(1.2, game.getPricePerDay());
+        assertFalse(game.isAvailable());
+    }
+    
+    
+}
